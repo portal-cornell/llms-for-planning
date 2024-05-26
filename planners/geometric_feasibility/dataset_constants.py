cc1c29c78dd3ba05fd6c9a49e2a0e0a9dfa93537
Neha Sunkara <ns597@cornell.edu>
2024-05-25 21:53:20 -0700

    Adds 37 new assets to folder and makes helper dict

--- planners/geometric_feasibility/dataset_constants.py
+++ planners/geometric_feasibility/dataset_constants.py
@@ -1,19 +1,20 @@
 # A list of at least 10 objects
 CATEGORY_DICT = {
-	"fruits": ["apple", "banana", "cherries", ….],
-	"dairy product": ["oat_milk", …
-	"juice and soft drinks": 
-	"condiments"
-	"vegetables"
+	"fruits": ["apple", "banana", "cherries", "orange", "pear", "watermelon", "papaya", "avocado", "mango", "pomegranate", "honeydew"],
+	"dairy product": ["almond_milk", "oat_milk", "whole_milk", "cheese_block", "yogurt", "butter", "egg", "sliced_cheese", "ice_cream", "string_cheese"],
+	"juice and soft drinks": ["fanta", "pepsi", "coca_cola", "ginger_ale", "gatorade", "strawberry_milk", "banana_milk", "orange_juice", "apple_juice", "cranberry_juice"],
+	"condiments": ["chocolate_sauce", "ketchup", "mustard", "salad_dressing", "soy_sauce", "hot sauce", "nutella", "maple_syrup", "whipped_cream", "peanut_butter"],
+	"vegetables": ["lettuce", "onion", "potato", "tomato", "eggplant", "pumpkin", "bell_pepper", "carrot", "radish", "corn"]
 }
 
 # A list of at least 3 objects 
 SUBCATEGORY_DICT = {
-    "heavy fruit": ["watermelon", "melon"]
-    "milk": ["oat milk", "whole milk"]
-    "juice"
-    "soft drinks"
-    "sweet condiments"
-    "savory condiments"
-    "heavy_vegetables"
+    "heavy fruit": ["watermelon", "papaya", "honeydew"],
+    "milk": ["almond_milk", "oat_milk", "whole_milk"],
+    "cheese": ["cheese_block", "sliced_cheese", "string_cheese"],
+    "juice": ["orange_juice", "apple_juice", "cranberry_juice"],
+    "soft drinks": ["fanta", "pepsi", "coca_cola", "ginger_ale"],
+    "sweet condiments": ["chocolate_sauce", "nutella", "maple_syrup", "whipped_cream", "peanut_butter"],
+    "savory condiments": ["ketchup", "mustard", "salad_dressing", "soy_sauce", "hot sauce"],
+    "heavy_vegetables": ["eggplant", "pumpkin", "corn"]
 }
