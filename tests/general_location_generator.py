96cafd0885c6811fc2a364b874a3b1da2dd1a934
chalo2000 <gg387@cornell.edu>
2024-05-31 02:07:57 -0400

    Update specific_location tests, implement general_location tests, and clean sample tests

--- tests/general_location_generator.py
+++ tests/general_location_generator.py
@@ -20,51 +20,80 @@ CATEGORY_DICT = {
 	"vegetables": ["lettuce", "onion", "potato", "tomato", "eggplant", "pumpkin", "bell pepper", "carrot", "radish", "corn"]
 }
 
-def generate_ground_truth_preference():
+def generate_ground_truth_preference(num_general_categories=1):
     """Returns a ground truth preference dictionary and string.
     
-    The ground truth preference should only contain specific locations for each object category.
+    The ground truth preference should contain specific locations for each object category
+    except for one category which is a general location.
+
+    General locations include:
+    - Top shelf
+    - Middle shelf
+    - Bottom shelf
+    - Left side of the fridge
+    - Right side of the fridge
 
     Example:
     Dict: {
-        'fruits': 'right side of top shelf',
+        'fruits': ['right side of top shelf', 'right side of middle shelf', 'right side of bottom shelf'],
         'dairy products': 'right side of top shelf',
         'juice and soft drinks': 'left side of bottom shelf',
         'condiments': 'right side of top shelf',
         'vegetables': 'left side of middle shelf'
     }
-    String: I want fruits on the right side of top shelf, dairy products on the right side of top shelf, juice and soft drinks on the left side of bottom shelf, condiments on the right side of top shelf, vegetables on the left side of middle shelf.
+    String: I want fruits on the right side of the fridge, dairy products on the right side of top shelf, juice and soft drinks on the left side of bottom shelf, condiments on the right side of top shelf, vegetables on the left side of middle shelf.
 
+    Parameters:
+        num_general_categories (int): The number of general categories to have.
+    
     Returns:
         gt_preference_dict (dict): The ground truth preference dictionary.
         gt_preference_str (str): The ground truth preference string.
+        general_categories (list): The general categories.
     """
     gt_preference_dict = {}
     chosen_locations = []
     categories = list(CATEGORY_DICT.keys())
     random.shuffle(categories)
+    general_location_idxs = random.sample(range(len(categories)), num_general_categories)
+    general_categories = [categories[idx] for idx in general_location_idxs]
     gt_preference_str = "I want "
     i = 0
     while len(chosen_locations) < len(categories):
-        direction = random.choice(["left", "right"])
-        shelf = random.choice(["top", "middle", "bottom"])
-        location = f"{direction} side of {shelf} shelf"
-        if location in chosen_locations:
-            continue
-        chosen_locations.append(location)
-        category = categories[i]
-        gt_preference_dict[category] = location
-        gt_preference_str += f"{category} on the {location}, "
+        if i in general_location_idxs:
+            location = random.choice(["top shelf", "middle shelf", "bottom shelf", "left side of fridge", "right side of fridge"])
+            if location in chosen_locations:
+                continue
+            chosen_locations.append(location)
+            category = categories[i]
+            # Location to list of categories
+            if "shelf" in location:
+                gt_preference_dict[category] = [f"left side of {location}", f"right side of {location}"]
+            else:
+                dir_side_of = location[:location.index(" fridge")]
+                gt_preference_dict[category] = [f"{dir_side_of} top shelf", f"{dir_side_of} middle shelf", f"{dir_side_of} bottom shelf"]
+            gt_preference_str += f"{category} on the {location}, "
+        else:
+            direction = random.choice(["left", "right"])
+            shelf = random.choice(["top", "middle", "bottom"])
+            location = f"{direction} side of {shelf} shelf"
+            if location in chosen_locations:
+                continue
+            chosen_locations.append(location)
+            category = categories[i]
+            gt_preference_dict[category] = location
+            gt_preference_str += f"{category} on the {location}, "
         i += 1
     gt_preference_str = gt_preference_str[:-2] + "."
-    return gt_preference_dict, gt_preference_str
+    return gt_preference_dict, gt_preference_str, general_categories
 
-def generate_demonstrations(env, gt_preference_dict, num_demonstrations=2, demo_objects=7, obj_to_place=4):
-    """Returns a list of demonstrations for the specific location preference.
+def generate_demonstrations(env, gt_preference_dict, general_categories, num_demonstrations=2, demo_objects=7, obj_to_place=4):
+    """Returns a list of demonstrations for the general location preference.
     
     Parameters:
         env (gym.Env): The environment to generate the demonstrations in.
         gt_preference_dict (dict): The ground truth preference dictionary.
+        general_categories (list): The categories with general location preferences.
         num_demonstrations (int): The number of demonstrations to generate.
         demo_objects (int): The number of objects to put away in each demonstration.
         obj_to_place (int): The number of objects to place in each demonstration.
@@ -76,8 +105,10 @@ def generate_demonstrations(env, gt_preference_dict, num_demonstrations=2, demo_
     demonstrations = []
     viz_data = []
     ordered_categories = list(CATEGORY_DICT.keys())
-    persistent_category = random.choice(ordered_categories) # Ensure one category always persists over demonstrations
-    ablated_locations = [] 
+    persistent_category = random.choice([category for category in ordered_categories if category not in general_categories]) # Ensure one specific location category always persists over demonstrations
+    general_category_ordering = [random.sample(gt_preference_dict[category], k=len(gt_preference_dict[category])) for category in general_categories]
+    general_category_indices = [0] * len(general_categories)
+    ablated_locations = []
     i = 0
     attempts = 300
     attempt_count = 0
@@ -94,13 +125,22 @@ def generate_demonstrations(env, gt_preference_dict, num_demonstrations=2, demo_
         
         # Generate the final state of the fridge
         text_plan = ""
+        locations = []
+        general_locations = {} # Category: Location
         for object, category in zip(all_objects, demo_categories):
             # Construct text plan
-            location = gt_preference_dict[category]
+            if category in general_categories:
+                category_idx = general_categories.index(category)
+                location = general_category_ordering[category_idx][general_category_indices[category_idx]]
+                general_locations[category] = location
+                general_category_indices[category_idx] = (general_category_indices[category_idx] + 1) % len(general_category_ordering[category_idx])
+            else:
+                location = gt_preference_dict[category]
+            locations.append(location)
             text_skill = f'pickandplace("{object}", "{location}")'
             text_plan += text_skill + "\n"
         text_plan = text_plan[:-1] # Remove last newline character
-        
+
         # Execute text plan
         env.reset()
 
@@ -115,24 +155,29 @@ def generate_demonstrations(env, gt_preference_dict, num_demonstrations=2, demo_
             continue
         
         final_state = {}
-        for action, obj_name in zip(best_action_sequence, best_obj_names):
+        for action, obj_name, location in zip(best_action_sequence, best_obj_names, locations):
             object_coord_pair = (obj_name, (action[0], action[1]))
             category = [category for category in ordered_categories if obj_name in CATEGORY_DICT[category]][0]
-            location = gt_preference_dict[category]
             final_state[location] = final_state.get(location, ()) + (object_coord_pair,)
         
         # Ablate the final state to generate the initial state
         initial_state = deepcopy(final_state)
         actual_ablations = []
         ablations = random.sample(list(gt_preference_dict.items()), k=len(gt_preference_dict))
+        # import pdb; pdb.set_trace()
         for category, location in ablations:
-            if location in ablated_locations and category != persistent_category:
+            if category in general_categories:
+                location = general_locations[category]
+            if location in ablated_locations and category not in general_categories and category != persistent_category:
                 # Should not ablate the same category (corresponding to location) twice to ensure coverage
                 # print(f"[-] Ablation {ablation} already performed. Skipping demonstration.")
                 continue
             temp_initial_state = initial_state.copy()
+            if len(temp_initial_state.get(location, ())) == 0:
+                # Skip ablation if the location is empty
+                continue
             temp_initial_state.pop(location)
-            if sum([len(objs) for objs in temp_initial_state.values()]) < demo_objects - obj_to_place and category != persistent_category:
+            if sum([len(objs) for objs in temp_initial_state.values()]) < demo_objects - obj_to_place and category not in general_categories and category != persistent_category:
                 # Skip ablation if it results in less than 'obj_to_place' objects
                 # print(f"[-] Ablation {ablation} resulted in {sum([len(objs) for objs in temp_initial_state.values()])} objects. Skipping demonstration.")
                 attempt_count += 1
@@ -160,12 +205,13 @@ def generate_demonstrations(env, gt_preference_dict, num_demonstrations=2, demo_
         return None, None
     return demonstrations, viz_data
 
-def generate_scenarios(env, gt_preference_dict, num_scenarios=2, scenario_objects=10, obj_to_place=6):
-    """Returns a list of scenarios for the specific location preference.
+def generate_scenarios(env, gt_preference_dict, general_categories, num_scenarios=2, scenario_objects=10, obj_to_place=6):
+    """Returns a list of scenarios for the general location preference.
     
     Parameters:
         env (gym.Env): The environment to generate the scenarios in.
         gt_preference_dict (dict): The ground truth preference dictionary.
+        general_categories (list): The categories with general location preferences.
         num_scenarios (int): The number of scenarios to generate.
         scenario_objects (int): The number of objects to put away in each scenario.
         obj_to_place (int): The number of objects to place in each scenario.
@@ -177,6 +223,8 @@ def generate_scenarios(env, gt_preference_dict, num_scenarios=2, scenario_object
     scenarios = []
     viz_data = []
     ordered_categories = list(CATEGORY_DICT.keys())
+    general_category_ordering = [random.sample(gt_preference_dict[category], k=len(gt_preference_dict[category])) for category in general_categories]
+    general_category_indices = [0] * len(general_categories)
     i = 0
     attempts = 100
     attempt_count = 0
@@ -193,9 +241,18 @@ def generate_scenarios(env, gt_preference_dict, num_scenarios=2, scenario_object
         
         # Generate the final state of the fridge
         text_plan = ""
+        locations = []
+        general_locations = {} # Category: Location
         for object, category in zip(all_objects, scenario_categories):
             # Construct text plan
-            location = gt_preference_dict[category]
+            if category in general_categories:
+                category_idx = general_categories.index(category)
+                location = general_category_ordering[category_idx][general_category_indices[category_idx]]
+                general_locations[category] = location
+                general_category_indices[category_idx] = (general_category_indices[category_idx] + 1) % len(general_category_ordering[category_idx])
+            else:
+                location = gt_preference_dict[category]
+            locations.append(location)
             text_skill = f'pickandplace("{object}", "{location}")'
             text_plan += text_skill + "\n"
         text_plan = text_plan[:-1] # Remove last newline character
@@ -213,25 +270,29 @@ def generate_scenarios(env, gt_preference_dict, num_scenarios=2, scenario_object
             continue # Regenerate the scenario if the plan is infeasible
         
         final_state = {}
-        for action, obj_name in zip(best_action_sequence, best_obj_names):
+        for action, obj_name, location in zip(best_action_sequence, best_obj_names, locations):
             object_coord_pair = (obj_name, (action[0], action[1]))
             category = [category for category in ordered_categories if obj_name in CATEGORY_DICT[category]][0]
-            location = gt_preference_dict[category]
             final_state[location] = final_state.get(location, ()) + (object_coord_pair,)
         
         # Ablate the final state to generate the initial state
         initial_state = deepcopy(final_state)
-        ablations = random.sample(list(gt_preference_dict.values()), k=len(gt_preference_dict))
-        for ablation in ablations:
+        ablations = random.sample(list(gt_preference_dict.items()), k=len(gt_preference_dict))
+        for category, location in ablations:
+            if category in general_categories:
+                location = general_locations[category]
             temp_initial_state = initial_state.copy()
-            ablate_amount = random.randint(1, len(temp_initial_state[ablation]))
-            temp_initial_state[ablation] = temp_initial_state[ablation][ablate_amount:]
+            if len(temp_initial_state.get(location, ())) == 0:
+                # Skip ablation if the location is empty
+                continue
+            ablate_amount = random.randint(1, len(temp_initial_state[location]))
+            temp_initial_state[location] = temp_initial_state[location][ablate_amount:]
             if sum([len(objs) for objs in temp_initial_state.values()]) < scenario_objects - obj_to_place:
                 # Skip ablation if it results in less than 'obj_to_place' objects
                 # print(f"[-] Ablation {ablation} resulted in {sum([len(objs) for objs in temp_initial_state.values()])} objects. Skipping scenario.")
                 attempt_count += 1
                 continue
-            initial_state[ablation] = initial_state[ablation][ablate_amount:]
+            initial_state[location] = initial_state[location][ablate_amount:]
         if sum([len(objs) for objs in initial_state.values()]) != scenario_objects - obj_to_place:
             # Regenerate the demonstration if the ablation results in less than 'obj_to_place' objects
             # print(f"[-] Ablation resulted in {sum([len(objs) for objs in initial_state.values()])} objects. Regenerating scenario.")
@@ -253,13 +314,13 @@ def generate_scenarios(env, gt_preference_dict, num_scenarios=2, scenario_object
         return None, None
     return scenarios, viz_data
 
-def specific_location_generator(num_tests, seed):
-    """Returns a dictionary of test cases for the specific location preference.
+def general_location_generator(num_tests, seed):
+    """Returns a dictionary of test cases for the general location preference.
     
     The test case format is as follows:
     {
         "0": {
-            "type": "specific_location",
+            "type": "general_location",
             "gt_preference": ...,
             "demonstrations": [
                 {
@@ -305,17 +366,17 @@ def specific_location_generator(num_tests, seed):
     tests = {}
     viz_data = {}
     for i in tqdm(range(num_tests)):
-        gt_preference_dict, gt_preference_str = generate_ground_truth_preference()
+        gt_preference_dict, gt_preference_str, general_category = generate_ground_truth_preference()
         demonstrations, demo_viz_data = None, None
         while demonstrations is None and demo_viz_data is None:
-            demonstrations, demo_viz_data = generate_demonstrations(env, gt_preference_dict, num_demonstrations=2, demo_objects=7, obj_to_place=4)
+            demonstrations, demo_viz_data = generate_demonstrations(env, gt_preference_dict, general_category, num_demonstrations=2, demo_objects=7, obj_to_place=4)
         # input(f"Generated demonstrations for Test {i}. Press Enter to continue.")
         scenarios, scenario_viz_data = None, None
         while scenarios is None and scenario_viz_data is None:
-            scenarios, scenario_viz_data = generate_scenarios(env, gt_preference_dict, num_scenarios=2, scenario_objects=10, obj_to_place=6)
+            scenarios, scenario_viz_data = generate_scenarios(env, gt_preference_dict, general_category, num_scenarios=2, scenario_objects=10, obj_to_place=6)
         # input(f"Generated scenarios for Test {i}. Press Enter to continue.")
         test = {
-            "type": "specific_location",
+            "type": "general_location",
             "gt_preference": gt_preference_str,
             "demonstrations": demonstrations,
             "scenarios": scenarios
@@ -382,22 +443,22 @@ def visualize_data(viz_data):
         viz_data (dict): The visualization data to visualize.
     """
     env = sim2d_utils.make_sim2d_env(render_mode="rgb_array")
-    os.makedirs("generated_tests/specific_location/demonstrations", exist_ok=True)
-    os.makedirs("generated_tests/specific_location/scenarios", exist_ok=True)
+    os.makedirs("generated_tests/general_location/demonstrations", exist_ok=True)
+    os.makedirs("generated_tests/general_location/scenarios", exist_ok=True)
     for i, viz in viz_data.items():
         demonstrations = viz["demonstrations"]
         scenarios = viz["scenarios"]
         for j, demonstration in enumerate(demonstrations):
-            save_replay_for_viz(env, demonstration, f"generated_tests/specific_location/demonstrations/test_{i}_demonstration_{j}.gif")
+            save_replay_for_viz(env, demonstration, f"generated_tests/general_location/demonstrations/test_{i}_demonstration_{j}.gif")
         for j, scenario in enumerate(scenarios):
-            save_replay_for_viz(env, scenario, f"generated_tests/specific_location/scenarios/test_{i}_scenario_{j}.gif")
+            save_replay_for_viz(env, scenario, f"generated_tests/general_location/scenarios/test_{i}_scenario_{j}.gif")
 
 if __name__ == "__main__":
-    tests, viz_data = specific_location_generator(10, 42)
+    tests, viz_data = general_location_generator(10, 42)
 
     # Write tests to file
-    os.makedirs("generated_tests/specific_location", exist_ok=True)
-    with open("generated_tests/specific_location/tests.json", "w") as file:
+    os.makedirs("generated_tests/general_location", exist_ok=True)
+    with open("generated_tests/general_location/tests.json", "w") as file:
         json.dump(tests, file, indent=4)
     
     # Visualize demonstrations and scenarios
